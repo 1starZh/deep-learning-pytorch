31c180686044d4459d43dfaf04727135df1c3683
1starZh <terryzhang518@outlook.com>
2025-09-29 15:36:18 +0800

    upd

--- ResNet.py
+++ ResNet.py
@@ -1,7 +1,53 @@
 import torch
 from torch import nn
 
-class ResNet50(nn.modules):
-    def __init__(self):
+num_cls = 10
+
+class Residual(nn.modules):
+    def __init__(self, input_channels, num_channels, use_1m1conv = False, strides=1):
         super().__init__()
-        
+        self.use_1m1conv = use_1m1conv
+        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        if use_1m1conv:
+            self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+
+        self.bn1 = nn.BatchNorm2d(num_channels)
+        self.bn2 = nn.BatchNorm2d(num_channels)
+
+    def forward(self, X):
+        Y = self.conv1(X)
+        Y = self.bn1(Y)
+        Y = self.conv2(Y)
+        Y = self.bn2(Y)
+        if self.use_1m1conv:
+            Y += self.conv3(X)
+        else:
+            Y += X
+        return nn.ReLU(Y)
+
+b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                   nn.BatchNorm2d(64), nn.ReLU(),
+                   nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+def resnet_block(input_channels, num_channels, num_residuals, first_block):
+    blk = []
+    for i in range(num_residuals):
+        if i == 0 and not first_block:
+            blk.append(Residual(input_channels, num_channels, use_1m1conv=True, strides=2))
+        else:
+            blk.append(Residual(num_channels, num_channels))
+    return blk
+
+b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
+b3 = nn.Sequential(*resnet_block(64, 128, 2))
+b4 = nn.Sequential(*resnet_block(128, 256, 2))
+b5 = nn.Sequential(*resnet_block(256, 512, 2))
+
+net = nn.Sequential(b1, b2, b3, b4, b5, 
+                    nn.AdaptiveAvgPool2d((1, 1)),
+                    nn.Flatten(),
+                    nn.Linear(512, num_cls))
+
